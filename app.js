d7d5e881098fd5728ad8e957f4509472018ff036
Rathirojini Sangilipandy <rathirojini.sangilipandy@capgemini.com>
2020-09-21 16:26:39 +0300

    font changes

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 (function () {
 'use strict';
 
-angular.module('lunchMenu',[])
+angular.module('LunchCheck',[])
 
-.controller ('countMyEatables' , function ($scope) {
+.controller ('LunchCheckController' , LunchCheckController);
+LunchCheckController.$inject= ['$scope'];
 
-$scope.name= "";
-$scope.message = "";
+function LunchCheckController ($scope)
+{
+  $scope.name= "";
+  $scope.message = "";
+  $scope.itemcolor = "";
 
-$scope.countMyfooditems = function () {
-  if ($scope.name == "")
-  {
-    $scope.message = "Please enter data first"
-  }
-  else
-  {
-  var foodmenu  = $scope.name.split(',');
-  var totalFoodItems = 0;
-  for( var i = 0 ; i < foodmenu.length ; i ++){
-    var items = foodmenu[i];
-    //to ignore the empty string
-    if (items.trim().length != 0){
-      totalFoodItems  = totalFoodItems + 1 ;
+  $scope.countMyfooditems = function () {
+    if ($scope.name == "") {
+      $scope.message = "Please enter data first";
+      $scope.itemcolor = "messagered";
+
+      }
+    else  {
+    var foodmenu  = $scope.name.split(',');
+    var totalFoodItems = 0;
+    for( var i = 0 ; i < foodmenu.length ; i ++){
+      var items = foodmenu[i];
+      //to ignore the empty string
+      if (items.trim().length != 0){
+        totalFoodItems  = totalFoodItems + 1 ;
+      }
     }
-  }
+    if (totalFoodItems <= '3')   {
+      $scope.message = "Enjoy !";
+      $scope.itemcolor = "messagegreen";
+    }
 
-  if (totalFoodItems <= '3')
-  {
-    $scope.message = "Enjoy !"
-  }
-  else
-  {
-      $scope.message = "Too much!"
-  }
-  }
-};
-
-});
-
-
+    else { $scope.message = "Too much!" ;
+    $scope.itemcolor = "messagered";
+ }
+    }
+  };
+}
 })()
